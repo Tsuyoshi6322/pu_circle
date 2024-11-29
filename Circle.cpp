79e206e17ee8e49b8a80aacda6fd4eb0ff5d23f3
Tsuyoshi <125825542+Tsuyoshi6322@users.noreply.github.com>
2024-11-29 13:00:45 +0100

    Final version of Cicle.cpp
    - No more nested classes
    - Comments locale normalized
    - Added Error Handlers to handle rule-breaking and program-breaking inputs
    - Added fancy outputs
    - Loops to give user unlimited chances to fix their input, if errored

--- Circle.cpp
+++ Circle.cpp
@@ -1,25 +1,16 @@
-/*
-    TODO:
-    - User's input
-    - ErrorHandlers
-        - Loops on user's input to not break the rules of the program
-        - Try,Catch 's to e.g. not divide by 0
-
-    TODO (later):
-    - Class optimalisation, generalisation
-        - Repetitive instructions, fix it
-        - Usless nesting of class' constructors, which could've been just methods
-    - Maybe make a separate version of the file, based on headers
-    - Some fancy output delay
-        - Like, print the lines character by character :D
-*/ 
-
 #include <iostream>
-#include <cmath>
+// potrzebne dla VectorChecker()
+#include <cmath> 
+// potrzebne do GetValidatedInput()
+#include <sstream>
+#include <string>
+#include <limits>
+    
 using namespace std;
 
 /*//================================================================================
     Definiowanie zmiennych punktów jako zbioru współrzędnych
+    Przyda się również w sytuacji wektorów
 *///================================================================================
 
 struct Point {
@@ -31,132 +22,170 @@ struct Point {
     Metody błędów:
         NonNumerErr() - Zła liczba podana jako input
         WrongAngle() - Wartość kąta alpha wykracza poza założenia programu
-        VectorVerifFailed() - WektorDE nie spełnia założeń zadania, 
-            nie jest pięciokrotnie mniejszy od promienia
+        VectorVerifFailed() - WektorDE nie spełnia założeń zadania (VectorDE * 5 = r)
 *///================================================================================
 
 class ErrorHandlers {
     public:
-    void NonNumberErr() {   
-        cout << "To nie jest liczba!\n";
-    }; 
+
+    float GetValidatedInput(const std::string& prompt) {
+        std::string input;
+        float value;
+
+        while (true) {
+            cout << prompt;
+            getline(cin, input);
+
+            // Konwersja tekstu na liczbę
+            stringstream stream(input);
+            if (stream >> value && stream.eof()) {
+                return value;
+            } else {
+                cout << "!!! To nie jest liczba, albo użyłeś przecinka jako separatora dziesiętnego - Użyj kropki !!!\n\n";
+            }
+        }
+    }
+
+    void SamePoints() {
+        cout << "!!! Punkty A i B nie mogą leżeć w tym samym miejscu !!!\n\n";
+    }
+
+    void DivZero(){
+        cout << "!!! Punkty A i B leżą na osi OX !!!\n";
+        cout << "!!! Nie będzie możliwe wyznaczenie punktu F (Dzielenie przez zero) !!!\n\n";
+    }
 
     void WrongAngle() {
-        cout << "Kąt ma być w zakresie 10-45 stopni!\n";
+        cout << "!!! Kąt ma być w zakresie 10-45 stopni !!!\n\n";
+    }
+
+    void WrongRadius() {
+        cout << "!!! Podaj liczbę większą niż 0 !!!\n\n";
     }
 
     void VectorVerifFailed() {
-        cout << "Vectors could not be verified, there is something wrong with the calculations\n";
+        cout << "!!! Długość wektora DE nie spełnia warunku zadania - błąd w obliczeniach !!!\n\n";
         exit(100);
     }
 };
 
 /*//================================================================================
     Wyznaczenie punktu D - operacje:
-        VectorAB::Basic - konstruktor wektora AB:
+        VectorAB() - metoda wyznaczająca wektor AB:
             - Współrzędne x, y wektora AB
             - Długość wektora AB
+            - Normalizacja wektora AB
 
-        VectorAB::Unified - kontruktor znormalizowanego wektora AB:
-            - Współrzędne x, y znormalizowanego wektora AB
-
-        CalcPointD - wyznaczenie współrzędnych punktu D:
+        Calculate() - metoda wyznaczająca współrzędne punktu D:
             - Współrzędne x, y punktu D
 *///================================================================================
 
 class CalcPointD {
     public:
 
-    float x, y, length;
+    Point UvAB;
 
-    void VectorAB(
-        Point A, Point B, float& x, float& y, float length){
-        x = B.x - A.x;
-        y = B.y - A.y;
-        length = sqrt(pow(x,2) + pow(y,2));
-        cout << "BasicAB: (" << x << "),(" << y << ")\n";
-        cout << "BasicAB.length: " << length << endl;
+    void VectorAB(Point* A, Point* B,  Point* UvAB){
+        // Współrzędne Wektora AB
+        Point vAB;
+        vAB.x = B->x - A->x;
+        vAB.y = B->y - A->y;
+        cout << "-------------------------------------------------------------------\n";
+        cout << "== Wektor AB: (" << vAB.x << "),(" << vAB.y << ")\n";
 
-        x = x / length;
-        y = y / length;
-        cout << "UnifiedAB: (" << x << "),(" << y << ")\n";
+        // Długość Wektora AB
+        float length;
+        length = sqrt(pow(vAB.x,2) + pow(vAB.y,2));
+        cout << "== Długość Wektora AB = " << length << endl;
+
+        // Normalizacja (Unifikacja) Wektora AB
+        UvAB->x = vAB.x / length;
+        UvAB->y = vAB.y / length;
+        cout << "== Znormalizowany Wektor AB: (" << UvAB->x << "),(" << UvAB->y << ")\n";
     };
 
-    void Calculate(Point D, Point A, float r, float x, float y){
-        D.x = A.x + (r * x);
-        D.y = A.y + (r * y);
-        cout << "PointD: (" << D.x << "),(" << D.y << ")\n";
+    void Calculate(Point* D, Point* A, float r, Point* UvAB){
+        // Współrzędne punktu D
+        D->x = A->x + (r * UvAB->x);
+        D->y = A->y + (r * UvAB->y);
+        //cout << "== PointD: (" << D->x << "),(" << D->y << ")\n";
+        cout << "-------------------------------------------------------------------\n";
     };
 };
 
 /*//================================================================================
     Wyznaczenie punktu E - operacje:
-        VectorAD - konstruktor wektora AD:
+        VectorAD() - metoda wyznaczająca wektor AD:
             - Współrzędne x, y wektora AD
             - Długość wektora AD
-
-        (Wektor AD należy obrócić (DE_1) i zastosować jego negację (DE_2), 
-        aby punkt E znajdował się wewnątrz okręgu o środku A)
         
-        VectorDE::Basic - konstruktor obróconego wektora AD:
-            - Współrzędne x, y (Obrócenie wektora AD o kąt alpha w lewo)
-            - Długość wektora DE_1 (Pięciokrotnie mniejszy od długości wektora AD)
+        VectorDE() - metoda wyznaczająca wektor DE:
+            - Współrzędne x, y wektora DE (Obrócenie wektora AD o kąt alpha w lewo)
+            - Długość wektora DE (Do późniejszej weryfikacji z założeniami zadania)
+            - Negacja współrzędnych wektora DE (Aby punkt E znajdował się wewnątrz okręgu o środku A)
 
-        VectorDE::Basic.VectorChecker() - sprawdzenie założenia DE*5 = AD
-            - Jeśli prawda -> idź dalej
-            - Jeśli fałsz -> terminacja programu
-
-        VectorDE - konstruktor wektora DE (DE_2):
-            - Współrzędne x, y wektora DE (Negacja obróconego wektora AD)
+        VectorChecker() - metoda sprawdzająca założenie WektorDE * 5 = AD = r
+            - Jeśli prawda -> idź dalej (Floating Point error w granicy 0.0001 jest dopuszczalny)
+            - Jeśli fałsz -> terminacja programu (kod 100)
         
-        CalcPointE - wyznaczenie współrzędnych punktu E:
+        Calculate() - wyznaczenie współrzędnych punktu E:
             - Współrzędne x, y punktu E
 *///================================================================================
 
 class CalcPointE {
     public:
 
-        float x, y, length;
+    Point vAD, vDE;
+    float length;
 
-        void VectorAD(float& x, float& y, Point D, Point A, float& length){
-            x = D.x - A.x;
-            y = D.y - A.y;
-            length = sqrt(pow(x,2) + pow(y,2));
+    void VectorAD(Point* D, Point* A, Point* vAD){
+        // Współrzędne Wektora AD
+        vAD->x = D->x - A->x;
+        vAD->y = D->y - A->y;
+        cout << "== Wektor AD: (" << vAD->x << "), (" << vAD->y << ")" << endl;
 
-            cout << "VectorAD: " << x << ", " << y << endl;
-            cout << "VectorAD.length: " << length << endl;
+        // Długość Wektora AD
+        float lengthAD;
+        lengthAD = sqrt(pow(vAD->x,2) + pow(vAD->y,2));
+        cout << "== Długość Wektora AD = " << lengthAD << endl;
+    };
+
+    void VectorDE(Point* vAD, Point* vDE, float alpha, float& length){
+        // Obrócenie Wektora AD o alpha kątów w lewo, pomniejszenie wartości pięciokrotnie
+        vDE->x = (vAD->x * cos(alpha) - vAD->y * sin(alpha)) / (5);
+        vDE->y = (vAD->x * sin(alpha) + vAD->y * cos(alpha)) / (5);
+        
+        // Wyliczenie długości wektora w celu weryfikacji
+        length = sqrt(pow(vDE->x,2) + pow(vDE->y,2));
+
+        // Negacja Wektora DE, aby punkt E znajdował się wewnątrz okręgu
+        vDE->x = -vDE->x;
+        vDE->y = -vDE->y;
+        cout << "== Wektor DE: (" << vDE->x << "), (" << vDE->y << ")" << endl;
+    };
+
+    void VectorChecker(float& length, float r){
+        // Floating point error handler
+        float epsilon = 0.0001;
+
+        // Jeżeli długość Wektora DE * 5 jest względnie taka sama z promieniem r to przejdź dalej
+        if (fabs((length * 5) - r) < epsilon) {
+            cout << "== Długość wektora DE spełnia warunek zadania!\n";
+        } 
+        // W przeciwnym razie, przerwij działanie programu
+        else {
+            ErrorHandlers ErrHand;
+            ErrHand.VectorVerifFailed();
         };
+    };
 
-        void VectorDE(float& x, float& y, float& alpha, float& length){
-            x = (x * cos(alpha) - y * sin(alpha)) / (5);
-            y = (x * sin(alpha) + y * cos(alpha)) / (5);
-            length = sqrt(pow(x,2) + pow(y,2));
-
-            cout << "BasicDE: " << x << ", " << y << endl;
-            cout << "BasicDE.length: " << length << endl;
-
-            x = -x;
-            y = -y;
-
-            cout << "VectorDE: " << x << ", " << y << endl;
-        };
-
-        void VectorChecker(float& length, float& r){
-            float epsilon = 0.00001;
-            if ((length * 5) == r) {
-                cout << "Vectors verified, DE is 5 times less than AB\n";
-            } else {
-                ErrorHandlers ErrHand;
-                ErrHand.VectorVerifFailed();
-            };
-        };
-
-        void Calculate(Point E, const Point D, float& x, float& y){
-            E.x = D.x + x;
-            E.y = D.y + y;
-
-            cout << "PointE: " << E.x << ", " << E.y << endl;
-        };
+    void Calculate(Point* E, Point* D, Point* vDE){
+        // Współrzędne punktu E
+        E->x = D->x + vDE->x;
+        E->y = D->y + vDE->y;
+        //cout << "== PointE: " << E->x << ", " << E->y << endl;
+        cout << "-------------------------------------------------------------------\n";
+    };
 };
 
 /*//================================================================================
@@ -165,82 +194,115 @@ class CalcPointE {
         Prosta f ma wzór ogólny f: y = mx + b)
         
         (Wzór na współrzędne punktu F został wyznaczony następująco:
-            - Równanie prostej prostopadłej do f: y = mx + b
-            - Wyznaczenie wyrazu wolnego b = y + x/m
+            - Wyznaczenie współczynnika kierunkowego na podstawie punktów A i B
+                m = (By - Ay) / (Bx - Ax)
+            - Wyznaczenie wyrazu wolnego b dla f: Ay = m * Ax + b
+                b = Ay - (m * Ax)
             - Równanie prostej przechodzącej przez punkt E, prostopadłej do prostej f
-                g: y = -x/m + yE + xE/m
+                g: y - Ey = -x/m + Ex/m
             - Wyznaczenie punktu przecięcia prostych f i g
                 P: f = g
-                P: mx + b = -x/m + yE + xE/m
-                P:  {xP = (xE + myE - mb) / (m^2 + 1)}
-                    {yP = mxP + b}                      // podstawienie xP do wzoru prostej f
-            - Odbicie symetryczne punktu E (odległość punktu E od P == odległość punkt F od P
-                F:  {xF = 2xP - xE}
-                    {yF = 2yP - yE}
-            - Po podstawieniu wzorów na xP w równaniu punktu F, wychodzą nam wzory w CalcPointF())
+                P: m * x + b = -x/m + Ey + Ex/m
+                P:  {Px = (Ex + m * Ey - m * b) / (m^2 + 1)} // podstawienie Px do wzoru prostej g
+                    {Py = m * Px + b}                      // podstawienie Py do wzoru prostej f
+            - Odbicie symetryczne punktu E (odległość punktu E od P == odległość punkt F od P)
+                F:  {Fx = 2Px - Ex}
+                    {Fy = 2Py - Ey}
+            - Po podstawieniu wzorów na Px w równaniu punktu F, wychodzą nam wzory w metodzie Calculate())
 
-        - LinearAB - konstruktor współczynnika kierunkowego m i wyrazu wolnego b:
+        - LinearAB() - metoda wyznaczająca współczynnik kierunkowy m i wyraz wolny b:
             - Zmienne m, b
         
-        - CalcPointF - wyznaczenie współrzędnych punktu F:
+        - Calculate() - metoda wyznaczająca współrzęde punktu F:
             - Współrzędne x, y punktu F
 *///================================================================================
 class CalcPointF {
     public:
-        float b, m;
+    float b, m;
 
-        void LinearAB(Point A, Point B, float& m, float& b){
-            m = (B.y - A.y) / (B.x - A.x);
-            b = A.y - (m * A.x);
+    void LinearAB(Point* A, Point* B, float& m, float& b){
+        // Współ. kierunkowy m oraz wyraz wolny b funkcji f: y = mx + b (na której leży odcinek AD)
+        m = (B->y - A->y) / (B->x - A->x);
+        b = A->y - (m * A->x);
+        cout << "== Współczynnik kierunkowy m = " << m << endl; 
+        cout << "== Wyraz wolny b = " << b << endl; 
+    };
 
-            cout << "LinearAB m,b: " << m << ", " << b << endl;
-        };
-
-        void Calculate(Point E, Point F, float& m, float& b){
-            F.x = 
-            ((1 - pow(m,2)) * E.x + (2 * m * E.y) - (2 * m * b)) 
-            / (1 + pow(m,2));
-
-            F.y = 
-            (((pow(m,2) - 1) * E.y) + (2 * m * E.x) + (2 * b))
-            /(1 + pow(m,2));
-
-            cout << "PointF: " << F.x << ", " << F.y << endl;
-        };
+    void Calculate(Point* E, Point* F, float& m, float& b){
+        // Współrzędne punktu F
+        F->x = ((1 - pow(m,2)) * E->x + (2 * m * E->y) - (2 * m * b)) / (1 + pow(m,2));
+        F->y = (((pow(m,2) - 1) * E->y) + (2 * m * E->x) + (2 * b)) / (1 + pow(m,2));
+        //cout << "== PointF: " << F->x << ", " << F->y << endl;
+    };
 };
 
 int main() {
     
 // Error Handlery zawarte w klasie ErrorHandlers
-// Przydatne do loopów inputowych, aby użytkownik podał poprawne wartości
     ErrorHandlers ErrHand;
-   // ErrHand.NonNumberErr();
+
+// TEST INPUT ===============================
+    // Point A = {2,3};
+    // Point B = {4,6};
+    // float r = 2;
+    // float angle = 30;
+// TEST INPUT ===============================
 
 // INPUT ====================================
-// Do przepisania na cin
-    Point A;
-    cout << "Podaj współrzędne x i y dla Punktu A: ";
-    cin >> A.x >> A.y;
-    
-    Point B;
-    cout << "Podaj współrzędne x i y dla Punktu B: ";
-    cin >> B.x >> B.y;
-    
-    float r; // (nie może być ujemne)
-    cout << "Podaj dłuość r: ";
-    cin >> r;
-        while (r < 0){
-            cout << "Podaj liczbę większą niż 0\n";
-            cin >> r;
+// Punkty A i B  
+    Point A, B; 
+    while (true){
+
+        A.x = ErrHand.GetValidatedInput("Podaj współrzędną x dla Punktu A: ");
+        A.y = ErrHand.GetValidatedInput("Podaj współrzędną y dla Punktu A: ");
+        cout << "Współrzędne punktu A: (" << A.x << ", " << A.y << ")\n";
+
+        B.x = ErrHand.GetValidatedInput("Podaj współrzędną x dla Punktu B: ");
+        B.y = ErrHand.GetValidatedInput("Podaj współrzędną y dla Punktu B: ");
+        cout << "Współrzędne punktu B: (" << B.x << ", " << B.y << ")\n";
+
+
+        if(A.x == B.x && A.y == B.y){
+            ErrHand.SamePoints();
+            continue;
         }
-    
-    float angle; // (angle <10,45>)
-    cout << "Podaj kąt: ";
-    cin >> angle;
-        while (angle < 10 || angle > 45){
+
+        if(A.x == 0 && B.x == 0){
+            ErrHand.DivZero();
+            continue;
+        }
+
+        break;
+    }
+
+// Promień okręgu - nie może być ujemny
+    float r; 
+    while (true){
+        r = ErrHand.GetValidatedInput("Podaj promień okręgu: ");
+
+        if (r <= 0) {
+            ErrHand.WrongRadius();
+            continue;
+        }
+
+        cout << "Promień okręgu to: " << r << endl;
+        break;
+    }
+
+// Kąt pomiędzy ramieniem strzałki a promieniem - w zakresie angle <10,45>
+    float angle;
+    while (true){
+        angle = ErrHand.GetValidatedInput("Podaj kąt (10-45): ");
+
+        if (angle < 10 || angle > 45){
             ErrHand.WrongAngle();
-            cin >> angle;
+            continue;
         }
+
+        cout << "Wybrany kąt to: " << angle << endl;
+        break;
+    }
+
 // ==========================================
 
 // Przekształcenie wartości kątowej na radiany 
@@ -252,32 +314,32 @@ int main() {
     Point D; 
     CalcPointD CalcPointD;
 
-    CalcPointD.VectorAB(A, B, CalcPointD.x, CalcPointD.y, CalcPointD.length);
-    CalcPointD.Calculate(D, A, r, CalcPointD.x, CalcPointD.y);
+    CalcPointD.VectorAB(&A, &B, &CalcPointD.UvAB);
+    CalcPointD.Calculate(&D, &A, r, &CalcPointD.UvAB);
 
 // Wyznaczenie Punktu E
     Point E; 
     CalcPointE CalcPointE;
 
-    CalcPointE.VectorAD(CalcPointE.x, CalcPointE.y, D, A, CalcPointE.length);
-    CalcPointE.VectorDE(CalcPointE.x, CalcPointE.y, alpha, CalcPointE.length);
+    CalcPointE.VectorAD(&D, &A, &CalcPointE.vAD);
+    CalcPointE.VectorDE(&CalcPointE.vAD, &CalcPointE.vDE, alpha, CalcPointE.length);
     CalcPointE.VectorChecker(CalcPointE.length, r);
-    CalcPointE.Calculate(E, D, CalcPointE.x, CalcPointE.y);
+    CalcPointE.Calculate(&E, &D, &CalcPointE.vDE);
 
 // Wyznaczenie Punktu F
     Point F;
     CalcPointF CalcPointF;
 
-    CalcPointF.LinearAB(A, B, CalcPointF.m, CalcPointF.b);
-    CalcPointF.Calculate(E, F, CalcPointF.m, CalcPointF.b);
+    CalcPointF.LinearAB(&A, &B, CalcPointF.m, CalcPointF.b);
+    CalcPointF.Calculate(&E, &F, CalcPointF.m, CalcPointF.b);
 
 // Output danych wyjściowych
-    cout << "PointD: (" << D.x << ", " << D.y << ")" << endl;
-    cout << "PointE: (" << E.x << ", " << E.y << ")" << endl;
-    cout << "PointF: (" << F.x << ", " << F.y << ")" << endl;
+    cout << "===================================================================\n";
+    cout << "== Współrzędne punktu D: (" << D.x << ", " << D.y << ")" << endl;
+    cout << "== Współrzędne punktu E: (" << E.x << ", " << E.y << ")" << endl;
+    cout << "== Współrzędne punktu F: (" << F.x << ", " << F.y << ")" << endl;
+    cout << "===================================================================\n";
 
-// KONIEC :DDDDDDD
+// KONIEC :D
     return 0;
 }       
-
-// yay line 300
